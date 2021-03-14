cd4baf9a32d7ad6b9d5ec56288fda872f7e4c964
Taylor Vellios <taylorvellios@gmail.com>
2021-03-14 12:52:19 -0500

    Full CSV Runthrough - No File Writing Output Yet

--- PyPoll.py
+++ PyPoll.py
@@ -3,31 +3,57 @@
 import os
 import csv
 
-# LOAD FROM SYS - OS.PATH.JOIN WILL AUTOMATICALLY CONNECT A HIGHER FOLDER TO A FILEWITHIN
+# ---------LOAD FROM SYS - OS.PATH.JOIN WILL AUTOMATICALLY CONNECT A HIGHER FOLDER TO A FILEWITHIN
 file_to_load = os.path.join('Resources','election_results.csv')
 
-# WRITE TO TXT
+# ---------WRITE TO TXT
 file_to_write = os.path.join('analysis', 'election_analysis.txt')
+
 print()
+
+
+total_votes = 0
+Candidates = []
+candidate_votes = {}
+
+# ---------OPEN THE FILE, SEPARATE THE HEADER LINE, ITERATE THROUGH ROWS OF DATA
 with open(file_to_load,'r') as vote_data:
     reader = csv.reader(vote_data)
     headers = next(reader)
 
     print(headers)
-    Candidates = []
-    Votes = {}
+
     for i in reader:
-        if i[2] not in Candidates:
-            Candidates.append(i[2])
-    print(f"The Candidates Are: {', '.join(Candidates)}")
-    #for i in reader:
+        candidate_name = i[2]
+        total_votes += 1
+
+        if candidate_name not in Candidates:
+            Candidates.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        
+        candidate_votes[candidate_name] += 1
 
 
-
+    print()        
+    print(f"The Candidates Are: {', '.join(Candidates)}\n")
+    print(f"Total Number of Votes: {total_votes}")
+    print()
 
 vote_data.close()
 
+# ---------DICTIONARY COMPREHENSION TO CONVERT OUR VOTECOUNT DICTIONARY PER CANDIDATE INTO A PERCENTAGE OF TOTAL VOTE PER CANDIDATE
+voting_percents = {k:((v / total_votes) * 100) for k,v in candidate_votes.items()}
+
+
+# ---------LOOP TO DISPLAY EACH CANDIDATES % OF VOTE AND TOTAL COUNT
+print("------------------------------------------------------------------------------")
+for k,v in voting_percents.items():
+    print(f"{k}: received {round(v,2)}% of the vote with {candidate_votes[k]} votes.")
+
+print("------------------------------------------------------------------------------\n")
+
+
 with open(file_to_write, 'w') as OutputFile:
 
-    OutputFile.write('Counties in the Election\n------------------------\nArapahoe\nDenver\nJefferson')
+    OutputFile.write("")
 OutputFile.close()
