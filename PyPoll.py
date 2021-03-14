5294d472fb2bdc85394cb6b7c9caeae49d2eec19
Taylor Vellios <taylorvellios@gmail.com>
2021-03-14 14:34:32 -0500

    Completed - County Data Output With Line Formatting

--- PyPoll.py
+++ PyPoll.py
@@ -11,10 +11,12 @@ file_to_write = os.path.join('analysis', 'election_analysis.txt')
 
 print()
 
-
+# ---------DECLARE THE GLOBAL VARIABLES
 total_votes = 0
 Candidates = []
 candidate_votes = {}
+counties = []
+county_votes = {}
 
 # ---------OPEN THE FILE, SEPARATE THE HEADER LINE, ITERATE THROUGH ROWS OF DATA
 with open(file_to_load,'r') as vote_data:
@@ -24,14 +26,23 @@ with open(file_to_load,'r') as vote_data:
     print(headers)
 
     for i in reader:
+        county_name = i[1]
         candidate_name = i[2]
         total_votes += 1
 
+        # -----------PULL UNIQUE CANDIDATE NAMES, INITIALIZE DICTIONARY COUNTS FOR EACH CANDIDATE    
         if candidate_name not in Candidates:
             Candidates.append(candidate_name)
             candidate_votes[candidate_name] = 0
         
+        # -----------PULL UNIQUE COUNTY NAMES, INITIALIZE DICTIONARY COUNTS FOR EACH COUNTY 
+        if county_name not in counties:
+            counties.append(county_name)
+            county_votes[county_name] = 0
+
+# -----------COUNT VOTES FOR EACH CANDIDATE/COUNTY
         candidate_votes[candidate_name] += 1
+        county_votes[county_name] += 1
 
 # -----------WRITING RESULTS TO TXT FILE       
 with open(file_to_write, 'w') as OutputFile:
@@ -46,22 +57,48 @@ with open(file_to_write, 'w') as OutputFile:
 
     print(election_results, end="")
     print()
+
     # Save the final vote count to the text file.
     OutputFile.write(election_results)
 
 
-# ---------DICTIONARY COMPREHENSION TO CONVERT OUR VOTECOUNT DICTIONARY PER CANDIDATE INTO A PERCENTAGE OF TOTAL VOTE PER CANDIDATE
+# -----------DICTIONARY COMPREHENSIONS TO CONVERT OUR DICTIONARIES WITH KEYS:COUNTS TO KEYS:%
     voting_percents = {k:((v / total_votes) * 100) for k,v in candidate_votes.items()}
+    counties_percents = {k:((v / total_votes)* 100) for k,v in county_votes.items()}
 
+
+# -----------INITIALIZE VARIABLES FOR VALUES TO PRINT TO TERMINAL/WRITE TO FILE 
     winner_vote_count = 0
     winner_vote_percent = 0.0
     winner_name = ""
 
-    # ---------LOOP TO DISPLAY EACH CANDIDATES % OF VOTE AND TOTAL COUNT - DETERMINE WINNER AND STORE IN VARIABLES
-    for cand, vote_p in voting_percents.items():
+    county_vote_count = 0
+    county_vote_percent = 0
+    largest_county = ''
 
-        #----------------CREATE FORMATTED VARIABLE, PRINT TO TERMINAL, WRITE TO OUTPUTFILE
-        candidate_results = (f"{cand}: {round(vote_p,1)}%  ({candidate_votes[cand]:,})\n")
+# -----------LOOP TO DISPLAY AND WRITE COUNTY RESULTS
+    space = len(max(counties)) + 2
+
+    for c , v in counties_percents.items():
+        inside_space = ' ' * (space - len(c))
+        county_results = (f'Votes in {c} County:{inside_space}{round(v,1)}% -- ({county_votes[c]:,})\n')
+        print(county_results)
+        OutputFile.write(county_results)
+
+        if (county_votes[c] > county_vote_count) and (v > county_vote_percent):
+            county_vote_count = county_votes[c]
+            county_vote_percent = v
+
+
+    print('-------------------------')
+    OutputFile.write('-------------------------\n')
+
+# -----------LOOP TO DISPLAY AND WRITE BALLOT RESULTS
+    space = len(max(Candidates)) + 7
+
+    for cand, vote_p in voting_percents.items():
+        inside_space = ' ' * (space - len(cand))
+        candidate_results = (f"{cand}:{inside_space}{round(vote_p,1)}% -- ({candidate_votes[cand]:,})\n")
         print(candidate_results)
         OutputFile.write(candidate_results)
 
@@ -70,6 +107,7 @@ with open(file_to_write, 'w') as OutputFile:
             winner_vote_count = candidate_votes[cand]
             winner_vote_percent = vote_p
             winner_name = cand
+    
 
     winning_candidate_summary = (
         f"-------------------------\n"
@@ -78,7 +116,6 @@ with open(file_to_write, 'w') as OutputFile:
         f"Winning Percentage: {winner_vote_percent:.1f}%\n"
         f"-------------------------\n")
     print(winning_candidate_summary)
-
 
 
     OutputFile.write(winning_candidate_summary)
